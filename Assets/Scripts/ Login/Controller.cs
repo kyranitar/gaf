abdbf98c7760274d5a6324fc976d34dd57a3e2b4
Richard Roberts <rykardo.r@gmail.com>
2011-12-03 20:51:06 +1300

    Library updates

---
+++ Assets/Scripts/ Login/Controller.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+<pre>public class Controller : MonoBehaviour {
+
+  public static string FacebookId = "1";
+  private const int max = 6;
+  private string btnText = "Press to roll";
+
+  void OnGUI() {
+
+    if (GUI.Button(new Rect(10, 10, 200, 50), btnText)) {
+      int score = UnityEngine.Random.Range(1, max + 1);
+      btnText = string.Format("You rolled {0}, \n click to try again.", score);
+
+      StartCoroutine(submitHighscore(score, FacebookId));
+    }
+  }
+
+  IEnumerator sumbitHighscore(int score, string fbid) {
+    yield return null;
+  }
