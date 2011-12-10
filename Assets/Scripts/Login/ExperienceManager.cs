e5a6dd25bb34391c2e033858d4ac7dcbd6bc24f2
Timothy Jones <git@zimothy.com>
2011-12-10 20:04:29 +1300

    Correcting compilation error in last commit.

--- Assets/Scripts/Login/ExperienceManager.cs
+++ Assets/Scripts/Login/ExperienceManager.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class ExperienceManager {
 
-  public static const string domain = "localhost";
-  public static const string ID = 643387554;
+  public static string domain = "localhost";
+  public static uint ID = 643387554;
 
   public static void AddExperience(uint amount) {
     new WWW("http://" + domain + "/add?id=" + ID + "&amount=" + amount);
