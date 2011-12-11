249f854e9d0ccb7118f7c1e3967975d5ad83c853
Cam Owen <camsnail@gmail.com>
2011-12-11 19:30:42 +1300

    Customise UI Update

--- Assets/Scripts/Customize/CustomizeGUI.cs
+++ Assets/Scripts/Customize/CustomizeGUI.cs
@@ -1,82 +1,247 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /// Handles the interface for the customize screen.
 /// In version three this will tell the Module Factories (ModuleFactory.cs) on the 'Ship' (if not 'Ship' it will be 'PlayerShip')
 /// what modules to create during the AddModule() method.
-public class CustomizeGUI : MonoBehaviour {
+public class CustomizeGUI : MonoBehaviour
+{
 
-  /// Mock idea for layout.
-  private Rect AddRemoveCoords = new Rect(25, 25, 100, 90);
-  private Rect addCoords;
-  private Rect removeCoords;
+  private int selectedUI;
 
-  private Rect toolbarCoords = new Rect(25, Screen.height - 55, 250, 30);
-  private string[] toolbarStrings;
+  private const int boxSpacing = 10;
+  private const float boxScreenWidth = 0.25f;
 
-  private int toolbarIndex = 0;
-  private ModuleFactory[] factories;
+  private Rect customiseRect;
+  private Rect[] weaponButtons;
+  private Rect[] skillButtons;
+  private Rect[] controlsButtons;
 
-  public void Start() {
+  private string[] toolbarStrings = new string[3] { "Weapons", "Skills", "Controls" };
+  private string[] weaponNames;
+  private string[] skillNames;
+  private string[] controlsNames;
+
+  // Number of Available weapons -- get from Factory
+  private int numWeapons;
+  // Number of Available skills -- get from Factory
+  private int numSkills;
+  // Number of Available control schemes -- get from Factory
+  private int numControls;
+
+  // TODO get this data from hull type?
+  // Number of weapons the ship can have
+  private const int maxWeapons = 2;
+  // Number of skills the ship can have
+  private const int maxSkills = 2;
+  // Number of control schemes the ship can have
+  private const int maxControls = 1;
+
+  // Size (diameter?) of buttons
+  private const int buttonSize = 45;
+  // Left offset to center the buttons
+  private int buttonLeftOffset = 0;
+  // Number of buttons per row in the button grid
+  private int buttonsPerRow;
+
+  private List<int> equippedWeapons = new List<int> ();
+  private List<int> equippedSkills = new List<int> ();
+  private List<int> equippedControls = new List<int> ();
+
+  private ModuleFactory weaponFactory;
+  private ModuleFactory skillFactory;
+  private ModuleFactory controlsFactory;
+
+  public void Start ()
+  {
+    // Show ship and enable factories
+    GameObject shipPrefab = GameObject.FindGameObjectWithTag ("ShipBlueprint");
+    
+    PlayerActivation activater = shipPrefab.GetComponent<PlayerActivation> ();
+    activater.SetFactoriesEnabled (true);
+    activater.Show ();
+    
+    
+    foreach (ModuleFactory mf in shipPrefab.GetComponents<ModuleFactory> ()) {
+      if (mf.FactoryType == "Weapon") {
+        weaponFactory = mf;
+      } else if (mf.FactoryType == "Skill") {
+        skillFactory = mf;
+      } else if (mf.FactoryType == "Control") {
+        controlsFactory = mf;
+      }
+    }
+    
+    // Set Factory-dependent variables
+    numWeapons = weaponFactory.Prefabs.Length;
+    numSkills = skillFactory.Prefabs.Length;
+    numControls = controlsFactory.Prefabs.Length;
+    
+    weaponNames = new string[numWeapons];
+    skillNames = new string[numSkills];
+    controlsNames = new string[numControls];
+    
+    for (int i = 0; i < weaponNames.Length; i++)
+      weaponNames[i] = weaponFactory.Prefabs[i].name;
+    for (int i = 0; i < skillNames.Length; i++)
+      skillNames[i] = skillFactory.Prefabs[i].name;
+    for (int i = 0; i < controlsNames.Length; i++)
+      controlsNames[i] = controlsFactory.Prefabs[i].name;
+    
     // TODO update this stuff to version 3.0
-    addCoords = new Rect(AddRemoveCoords.x + 10, AddRemoveCoords.y + 30, AddRemoveCoords.width * 0.8f, AddRemoveCoords.height * 0.2f);
-    removeCoords = new Rect(AddRemoveCoords.x + 10, AddRemoveCoords.y + 60, AddRemoveCoords.width * 0.8f, AddRemoveCoords.height * 0.2f);
-    GameObject shipPrefab = GameObject.FindGameObjectWithTag("ShipBlueprint");
-    factories = shipPrefab.GetComponents<ModuleFactory>();
-    toolbarStrings = new string[3] { "Weapons", "Skills", "Movement" };
-
-    PlayerActivation activater = shipPrefab.GetComponent<PlayerActivation>();
-    activater.SetFactoriesEnabled(true);
-    activater.Show();
+    
+    InitUI ();
   }
 
-  public void OnGUI() {
+  private void InitUI ()
+  {
+    int boxLeft = boxSpacing;
+    int boxTop = boxSpacing;
+    
+    int boxWidth = (int)(Camera.current.GetScreenWidth () * boxScreenWidth - boxSpacing);
+    
+    buttonsPerRow = Mathf.FloorToInt ((boxWidth + boxSpacing) / (buttonSize + boxSpacing));
+    
+    int boxHeight = (int)(Camera.current.GetScreenHeight () - boxSpacing * 2);
+    
+    customiseRect = new Rect (boxLeft, boxTop, boxWidth, boxHeight);
+    
+    buttonLeftOffset = ((boxWidth - boxSpacing) - (int)((buttonSize + boxSpacing) * Mathf.Floor ((boxWidth - boxSpacing) / (buttonSize + boxSpacing)))) / 2;
+    
+    // Create Individual Button Bounding Boxes
+    weaponButtons = new Rect[numWeapons];
+    MakeButtons (weaponButtons);
+    
+    skillButtons = new Rect[numSkills];
+    MakeButtons (skillButtons);
+    
+    controlsButtons = new Rect[numControls];
+    MakeButtons (controlsButtons);
+  }
 
+  private void MakeButtons (Rect[] rectArray)
+  {
+    int yOffset = boxSpacing * 4;
+    
+    for (int i = 0; i < rectArray.Length; i++) {
+      if (i % buttonsPerRow == 0 && i != 0)
+        yOffset += boxSpacing + buttonSize;
+      
+      int bLeft = buttonLeftOffset + (int)customiseRect.x + boxSpacing * ((i % buttonsPerRow) + 1) + buttonSize * (i % buttonsPerRow);
+      int bTop = yOffset;
+      
+      rectArray[i] = new Rect (bLeft, bTop, buttonSize, buttonSize);
+    }
+  }
+
+  public void OnGUI ()
+  {
+    
     // Update the toolbar.
-    toolbarIndex = GUI.Toolbar(toolbarCoords, toolbarIndex, toolbarStrings);
-    GUI.Box(AddRemoveCoords, toolbarStrings[toolbarIndex]);
-
-    if (GUI.Button(addCoords, "Add")) {
-      addObject(toolbarIndex);
+    selectedUI = GUI.Toolbar (new Rect (boxSpacing, boxSpacing, customiseRect.width, boxSpacing * 2), selectedUI, toolbarStrings);
+    GUI.Box (customiseRect, "");
+    
+    if (selectedUI == 0) {
+      for (int i = 0; i < weaponButtons.Length; i++)
+        if (GUI.Button (weaponButtons[i], weaponNames[i]))
+          ToggleWeapon (i);
+    } else if (selectedUI == 1) {
+      for (int i = 0; i < skillButtons.Length; i++)
+        if (GUI.Button (skillButtons[i], skillNames[i]))
+          ToggleSkill (i);
+    } else if (selectedUI == 2) {
+      for (int i = 0; i < controlsButtons.Length; i++)
+        if (GUI.Button (controlsButtons[i], controlsNames[i]))
+          ToggleControls (i);
     }
-
-    if (GUI.Button(removeCoords, "Remove")) {
-      removeObject(toolbarIndex);
-    }
-
-    if (Input.GetKeyDown(KeyCode.L)) {
-      Debug.Log(toolbarIndex);
-    }
-
-    if (GUI.Button(new Rect(100, 200, 50, 50), "Back")) {
-      GameObject shipPrefab = GameObject.FindGameObjectWithTag("ShipBlueprint");
-      PlayerActivation activater = shipPrefab.GetComponent<PlayerActivation>();
-      activater.Hide();
-      activater.SetFactoriesEnabled(false);
-      Application.LoadLevel("Star Map");
-    }
-
-    if (GUI.Button(new Rect(300, 200, 50, 50), "Turn on")) {
-      GameObject shipPrefab = GameObject.FindGameObjectWithTag("ShipBlueprint");
-      PlayerActivation activater = shipPrefab.GetComponent<PlayerActivation>();
-      activater.SetFactoriesEnabled(true);
-      activater.Show();
+    
+    Rect backButtonRect = new Rect (boxSpacing, customiseRect.height - boxSpacing, customiseRect.width, boxSpacing * 2);
+    
+    if (GUI.Button (backButtonRect, "Back")) {
+      GameObject shipPrefab = GameObject.FindGameObjectWithTag ("ShipBlueprint");
+      PlayerActivation activater = shipPrefab.GetComponent<PlayerActivation> ();
+      activater.Hide ();
+      activater.SetFactoriesEnabled (false);
+      Application.LoadLevel ("Star Map");
     }
   }
 
+  private void ToggleWeapon (int i)
+  {
+    if (equippedWeapons.Contains (i)) {
+      //TODO change icon rather than string
+      weaponNames[i] = ToggleString (weaponNames[i]);
+      equippedWeapons.Remove (i);
+      
+      weaponFactory.RemoveModuleByName (weaponNames[i]);
+      return;
+    }
+    if (equippedWeapons.Count >= maxWeapons)
+      ToggleWeapon (equippedWeapons[0]);
+    
+    equippedWeapons.Add (i);
+    weaponNames[i] = ToggleString (weaponNames[i]);
+    
+    weaponFactory.AddModule (i);
+  }
+
+  private void ToggleSkill (int i)
+  {
+    if (equippedSkills.Contains (i)) {
+      //TODO change icon rather than string
+      skillNames[i] = ToggleString (skillNames[i]);
+      equippedSkills.Remove (i);
+      
+      skillFactory.RemoveModuleByName (skillNames[i]);
+      return;
+    }
+    if (equippedSkills.Count >= maxSkills)
+      ToggleSkill (equippedSkills[0]);
+    
+    equippedSkills.Add (i);
+    skillNames[i] = ToggleString (skillNames[i]);
+    
+    skillFactory.AddModule (i);
+  }
+
+  private void ToggleControls (int i)
+  {
+    if (equippedControls.Contains (i)) {
+      //TODO change icon rather than string
+      controlsNames[i] = ToggleString (controlsNames[i]);
+      equippedControls.Remove (i);
+      
+      controlsFactory.RemoveModuleByName (controlsNames[i]);
+      return;
+    }
+    if (equippedControls.Count >= maxControls)
+      ToggleControls (equippedControls[0]);
+    
+    equippedControls.Add (i);
+    controlsNames[i] = ToggleString (controlsNames[i]);
+    
+    controlsFactory.AddModule (i);
+  }
+
+  private void RemoveByName (List<GameObject> objects, string name)
+  {
+    for (int i = 0; i < objects.Count; i++) {
+      if (objects[i].name.Contains (name)) {
+        objects.RemoveAt (i);
+        return;
+      }
+    }
+  }
+
+  string ToggleString (string s)
+  {
+    return s;
+  }
+  
   /* TODO for version 3.0
    *
    * Select our factory with the toolbar index (e.g. do we want to edit the weapons, skills, or movement).
    * There should then be a second arguement the specifies what to add or remove (e.g. add the rapid fire skill, provided we are in skill edit mode.
-   */
-  
-  /// Tells our factory what to add. This will eventually pass in two arguements.
-  private void addObject(int toolbarIndex) {
-    factories[toolbarIndex].AddModule();
-  }
-
-  /// Tells our factory what to remove. This will eventually pass in two arguements.
-  private void removeObject(int toolbarIndex) {
-    factories[toolbarIndex].RemoveModule();
-  }
+   */  
 }
--- Assets/Scripts/Customize/Factories/ModuleFactory.cs
+++ Assets/Scripts/Customize/Factories/ModuleFactory.cs
@@ -5,7 +5,8 @@ using System.Collections.ObjectModel;
 /// Makes a collection of modules for the player.
 /// By modules, we mean weapons, skills, or controls - each module is a unique prefab found in unity.
 /// Each factory is limited to produce only a set of weapons, skills, or controls.
-public class ModuleFactory : MonoBehaviour {
+public class ModuleFactory : MonoBehaviour
+{
 
   /// A list of possible skills avialable through the customize screen.
   public GameObject[] Prefabs;
@@ -19,46 +20,54 @@ public class ModuleFactory : MonoBehaviour {
   /// A list of all the currently installed modules on the players (e.g. weapons A, B, and C, or skills D, and E).
   private IList<GameObject> modules;
   public ReadOnlyCollection<GameObject> Modules {
-    get {
-      return new ReadOnlyCollection<GameObject>(modules);
-    }
+    get { return new ReadOnlyCollection<GameObject> (modules); }
   }
 
-  // TODO Remove this.
-  // Temporary index for what module to add and remove. This will be updated by the menu system in the customize screen, eventually.
-  private int currentIndex = 0;
-
-  public void Start() {
-    modules = new List<GameObject>();
+  public void Start ()
+  {
+    modules = new List<GameObject> ();
   }
 
   /// Adds a given module to the player (modules are specified in the unity interface, on the 'Ship' (if not ship, it will be 'PlayerShip') prefab).
-  public void AddModule() {
+  public void AddModule (int i)
+  {
     // TODO Remove this dependency on currentIndex and the if statement checking the bounds.
-    if (currentIndex >= Prefabs.Length) {
+    if (i >= Prefabs.Length) {
       return;
     }
-
-    GameObject module = Instantiate(Prefabs[currentIndex++], transform.position + Positions[modules.Count], transform.rotation) as GameObject;
+    
+    GameObject module = Instantiate (Prefabs[i], transform.position + Positions[modules.Count], transform.rotation) as GameObject;
     module.transform.parent = this.transform;
-    modules.Add(module);
-
+    modules.Add (module);
+    
     // Turn all of this new modules (prefabs) components off, to avoid things like shooting or skills activating in the menu scenes.
-    foreach (MonoBehaviour script in module.GetComponents<MonoBehaviour>()) {
+    foreach (MonoBehaviour script in module.GetComponents<MonoBehaviour> ()) {
       script.enabled = false;
     }
   }
 
   /// Removes a module based on the currentIndex (currentIndex will eventually reflect what the player has selected in the menu system).
-  public void RemoveModule() {
+  public void RemoveModule (int i)
+  {
     // TODO Remove this dependency on currentIndex and the if statement checking the bounds.
-    if (currentIndex <= 0) {
+    if (i <= 0) {
       return;
     }
-
-    GameObject module = modules[currentIndex--];
-    modules.Remove(module);
-    Destroy(module);
+    
+    GameObject module = modules[i];
+    modules.Remove (module);
+    Destroy (module);
   }
 
+  public void RemoveModuleByName (string name)
+  {
+    for (int i = 0; i < modules.Count; i++) {
+      if (modules[i].name.Contains (name)) {
+        Destroy (modules[i]);
+        modules.RemoveAt (i);
+        return;
+      }
+    }
+  }
+  
 }
